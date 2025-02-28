d1ae92f0673bbf5a6c571c0f497986600e731f93
Alessio <alessio.benvenuti99@gmail.com>
2025-02-28 18:42:29 +0100

    relative threejs import

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-import * as THREE from 'three';
+import * as THREE from './node_modules/three/build/three.module.js';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
