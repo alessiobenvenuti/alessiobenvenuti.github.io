56e1f3100a378f1eae6750202b3575923284b6f3
Alessio <alessio.benvenuti99@gmail.com>
2025-02-28 19:31:10 +0100

    updating files

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
